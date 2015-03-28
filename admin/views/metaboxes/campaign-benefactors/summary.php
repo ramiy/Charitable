d481f2711634f60eb62999683e1fc77c63f0db7b
Eric Daams <eric@ericnicolaas.com>
2015-03-28 23:03:11 +0930

    Added delete link to summary of benefactor relationship. Need to create JS to handle clicks on this.

--- admin/views/metaboxes/campaign-benefactors/summary.php
+++ admin/views/metaboxes/campaign-benefactors/summary.php
@@ -11,5 +11,8 @@ $benefactor 	= $view_args[ 'benefactor' ];
 ?>
 <div class="charitable-benefactor-summary">
 	<span class="summary"><?php echo $benefactor ?></span>
-	<a href="#" class="alignright" data-charitable-toggle="campaign_benefactor_<?php echo $benefactor->campaign_benefactor_id  ?>"><?php _e( 'Edit', 'charitable' ) ?></a>
+	<span class="alignright">
+		<a href="#" data-charitable-toggle="campaign_benefactor_<?php echo $benefactor->campaign_benefactor_id  ?>"><?php _e( 'Edit', 'charitable' ) ?></a>&nbsp;&nbsp;&nbsp;
+		<a href="#" data-campaign-benefactor-delete="campaign_benefactor_<?php echo $benefactor->campaign_benefactor_id  ?>"><?php _e( 'Delete', 'charitable' ) ?></a>
+	</span>
 </div>
