02b3a3ad6cbe31ebe0564424a8e73b0058bd5b49
Eric Daams <eric@164a.com>
2015-11-30 15:03:01 +0930

    Improved compatibility with WordPress 4.4. Fixed bugs with how the modal donation works when opened from the campaign grid.

--- charitable.php
+++ charitable.php
@@ -3,7 +3,7 @@
  * Plugin Name:         Charitable
  * Plugin URI:          https://www.wpcharitable.com
  * Description:         The WordPress fundraising alternative for non-profits, created to help non-profits raise money on their own website. 
- * Version:             1.2.4-beta20151126
+ * Version:             1.2.4-beta20151130
  * Author:              WP Charitable
  * Author URI:          https://wpcharitable.com
  * Requires at least:   4.1
