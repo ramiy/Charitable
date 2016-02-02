32e36fe3f87798116ffdbe267398230921eba022
Kathy Darling <helgatheviking@gmail.com>
2016-02-02 10:29:34 -0500

    deprecate static functions

--- includes/donations/charitable-donation-hooks.php
+++ includes/donations/charitable-donation-hooks.php
@@ -55,9 +55,9 @@ add_action( 'charitable_make_donation_streamlined', array( 'Charitable_Donation_
 /**
  * Donation update.
  * 
- * @see     Charitable_Donation::flush_campaigns_donation_cache
+ * @see charitable_flush_campaigns_donation_cache()
  */
-add_action( 'save_post_' . Charitable::DONATION_POST_TYPE, array( 'Charitable_Donation', 'flush_campaigns_donation_cache' ) );
+add_action( 'save_post_' . Charitable::DONATION_POST_TYPE, 'charitable_flush_campaigns_donation_cache' );
 
 /**
  * Delete a donation.
