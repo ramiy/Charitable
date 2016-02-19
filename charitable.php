f19734a003dbbc6f6b6c36afc4681f1691ad6521
Eric Daams <eric@164a.com>
2016-02-19 10:31:16 +0930

    Version bump and build for 1.3.1

--- charitable.php
+++ charitable.php
@@ -3,7 +3,7 @@
  * Plugin Name:         Charitable
  * Plugin URI:          https://www.wpcharitable.com
  * Description:         The WordPress fundraising alternative for non-profits, created to help non-profits raise money on their own website. 
- * Version:             1.3.0
+ * Version:             1.3.1
  * Author:              WP Charitable
  * Author URI:          https://wpcharitable.com
  * Requires at least:   4.1
@@ -26,14 +26,14 @@ if ( ! class_exists( 'Charitable' ) ) :
  * Main Charitable class
  *
  * @class       Charitable
- * @version     1.3.0
+ * @version     1.3.1
  */
 class Charitable {
 
     /**
      * @var     string
      */
-    const VERSION = '1.3.0';
+    const VERSION = '1.3.1';
 
     /**
      * @var     string A date in the format: YYYYMMDD
