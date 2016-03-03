77f0ad2bc3fc21912faa5ad6554f5b60ed5c636a
Eric Daams <eric@164a.com>
2016-03-03 22:00:00 +0930

    First steps towards supporting picture upload with the picture form field.

--- templates/form-fields/picture.php
+++ templates/form-fields/picture.php
@@ -16,6 +16,13 @@ $field          = $view_args[ 'field' ];
 $classes        = $view_args[ 'classes' ];
 $is_required    = isset( $field[ 'required' ] ) ? $field[ 'required' ] : false;
 $placeholder    = isset( $field[ 'placeholder' ] ) ? esc_attr( $field[ 'placeholder' ] ) : '';
+
+$size           = isset( $field[ 'size' ] ) ? $field[ 'size' ] : 'thumbnail';
+
+// Load all the media scripts.
+wp_enqueue_media();
+wp_enqueue_script( 'charitable-media-fields' );
+
 ?>
 <div id="charitable_field_<?php echo $field[ 'key' ] ?>" class="<?php echo $classes ?>">    
     <?php if ( isset( $field[ 'label' ] ) ) : ?>
@@ -26,9 +33,19 @@ $placeholder    = isset( $field[ 'placeholder' ] ) ? esc_attr( $field[ 'placehol
             <?php endif ?>
         </label>
     <?php endif ?>
-    <?php if ( strlen( $field[ 'value' ] ) ) : 
-        echo $field[ 'value' ]; 
-    endif; ?>
-        
-    <input type="file" name="<?php echo $field[ 'key' ] ?>" /> 
+    <div class="charitable-media-upload" 
+        data-size="<?php echo $media_size ?>"
+        data-user="<?php echo get_current_user_id() ?>"
+        data-key="<?php echo $field[ 'key' ] ?>"
+        data-change-label="<?php _e( 'Choose a different photo', 'charitable' ) ?>"
+        data-upload-label="<?php _e( 'Upload a photo', 'charitable' ) ?>" 
+        data-upload-title="<?php _e( 'Insert photo', 'charitable' ) ?>" 
+        >
+        <?php 
+        if ( strlen( $field[ 'value' ] ) ) : 
+            echo $field[ 'value' ]; 
+        endif; 
+        ?>
+        <input type="file" name="<?php echo $field[ 'key' ] ?>" /> 
+    </div>
 </div>
