542f0fee7efcb8b37f670da1ec906ea217c9f10d
Eric Daams <eric@164a.com>
2016-03-05 08:35:14 +0930

    First pass at using pupload instead of trying to piggyback on the WordPress media uploader. pupload looks like a much simpler option to implement.

--- includes/ajax/charitable-ajax-functions.php
+++ includes/ajax/charitable-ajax-functions.php
@@ -38,3 +38,47 @@ if ( ! function_exists( 'charitable_ajax_get_donation_form' ) ) :
         die();
     }
 endif;
+
+if ( ! function_exists( 'charitable_plupload_image_upload' ) ) :
+    /**
+     * Upload an image via plupload.
+     *
+     * @return
+     */
+    function charitable_plupload_image_upload() {
+        $post_id = (int) filter_input( INPUT_POST, 'post_id', FILTER_SANITIZE_NUMBER_INT );
+        $field_id = (string) filter_input( INPUT_POST, 'field_id' );
+
+        check_ajax_referer( 'charitable-upload-images-' . $field_id );
+
+        $file = $_FILES[ 'async-upload' ];
+        $file_attr = wp_handle_upload( $file, array( 'test_form' => false ) );
+        $attachment = array(
+            'guid'              => $file_attr[ 'url' ],
+            'post_mime_type'    => $file_attr[ 'type' ],
+            'post_title'        => preg_replace( '/\.[^.]+$/', '', basename( $file['name'] ) ),
+            'post_content'   => '',
+            'post_status'    => 'inherit',
+        );  
+
+        /**
+         * Insert the file as an attachment.
+         */
+        $attachment_id = wp_insert_attachment( $attachment, $file_attr[ 'file' ], $post_id );
+
+        if ( is_wp_error( $attachment_id ) ) {
+            wp_send_json_error();
+        }
+
+        wp_update_attachment_metadata( $attachment_id, wp_generate_attachment_metadata( $attachment_id, $file_attr[ 'file' ] ) );
+
+        /**
+         * Save the file ID in the meta.
+         */
+        // add_post_meta( $post_id, $field_id, $attachment_id, false );
+
+        wp_send_json_success( $attachment_id );
+    }
+endif;
+
+
