4774611682bdccb41e1a89f970995c7f2bd6e6b5
Eric Daams <eric@164a.com>
2016-03-07 18:26:44 +0930

    Fleshed out the image upload, removal and queue. Also consolidated the picture preview as a new template for a consistent interface. Basic styling added, but this should probably be moved into its own CSS file.

--- assets/js/charitable-pupload-fields.js
+++ assets/js/charitable-pupload-fields.js
@@ -1,42 +1,58 @@
-( function ( $ ) {
-    'use strict';
+( function( $ ){
 
-    console.log( CHARITABLE_UPLOAD_VARS );
+    $( '.charitable-drag-drop' ).each( function() {    
 
-    $( '.charitable-drag-drop' ).each( function() {
+        var self = this,
+            $dragdrop = $( this ),            
+            params = $dragdrop.data( 'params' ),
+            $images = $( '#' + $dragdrop.data( 'images' ) ),
+            $loader = $dragdrop.find( '.charitable-drag-drop-image-loader' ).first(),
+            max_file_uploads = params.multipart_params.max_uploads,
+            max_file_size = parseInt( $dragdrop.data( 'max-size' ), 10 ),
+            isIE = navigator.userAgent.indexOf('Trident/') != -1 || navigator.userAgent.indexOf('MSIE ') != -1,                    
+            msg = get_max_uploads_message( max_file_uploads ), 
+            $dropzone, key, error, uploader;
 
-        var $drop_area = $( this ),
-            $images = $drop_area.siblings( '.charitable-uploaded' ),
-            uploader_data = $drop_area.data( 'js-options' ),
-            uploader;
+        uploader = new plupload.Uploader( params );
+        
+        $dropzone = $( uploader.settings.drop_element );        
 
-        uploader_data.multipart_params = $.extend( {
-                '_ajax_nonce' : $drop_area.data( 'upload-nonce' ),
-               'post_id' : $drop_area.data( 'post-id' )
-            },
-            uploader_data.multipart_params
-        );
-
-        // Create the uploader
-        uploader = new plupload.Uploader( uploader_data );
         uploader.init();
 
+        uploader.bind( 'PostInit', function() {
+            
+            if ( ! $dropzone ) {
+                return;
+            }
+
+            $dropzone.addClass( 'supports-drag-drop' );
+
+            // We may need to enhance this to account for the issue noted
+            // in https://core.trac.wordpress.org/ticket/21705
+            $dropzone.bind( 'dragover', function(){
+                $dropzone.addClass('drag-over');
+            });
+
+            $dropzone.bind( 'dragleave', function(){
+                $dropzone.removeClass('drag-over');
+            });
+
+            // Set up image remove handler
+            $dragdrop.on( 'click', '.remove-image', remove_image );
+        });
+
         // Add files
-        uploader.bind( 'FilesAdded', function( up, files ) {
+        uploader.bind( 'FilesAdded', function( uploader, files ) {
 
-            var max_file_uploads = $images.data( 'max_file_uploads' ),
-                uploaded = $images.children().length,
-                msg = max_file_uploads > 1 ? CHARITABLE_UPLOAD_VARS.max_file_uploads_plural : CHARITABLE_UPLOAD_VARS.max_file_uploads_single;
-
-            msg = msg.replace( '%d', max_file_uploads );
+            var uploaded = $images.children().length;
 
             // Remove files from queue if the max number of files have been uploaded
             if ( max_file_uploads > 0 && ( uploaded + files.length ) > max_file_uploads ) {
 
                 if ( uploaded < max_file_uploads ) {
-                    var diff = max_file_uploads - uploaded.
-                    up.splice( diff - 1, files.length - diff );
-                    files = up.files;
+                    var diff = max_file_uploads - uploaded;
+                    uploader.splice( diff - 1, files.length - diff );
+                    files = uploader.files;
                 }
 
                 alert( msg );
@@ -44,42 +60,116 @@
 
             // Hide drag & drop section if we have reached the max number of file uploads.
             if ( max_file_uploads > 0 && uploaded + files.length >= max_file_uploads ) {
-                $drop_area.addClass( 'hidden' );
+                hide_dropzone( $dropzone );                
             }
-
-            var max = parseInt( up.settings.max_file_size, 10 );
 
             // Upload files
             plupload.each( files, function( file ) {
-                addLoading( up, file, $images );
-                addThrobber( file );
-                if ( file.size >= max ) {
+
+                add_image_loader( $loader, file );
+
+                // addLoading( uploader, file, $images );
+                // addThrobber( file );
+
+                if ( file.size >= max_file_size ) {
                     removeError( file );
                 }
             });
 
-            up.refresh();
-            up.start();
+            uploader.refresh();
+            uploader.start();
         });
 
-        uploader.bind( 'Error', function( up, e ){
+        uploader.bind( 'Error', function( uploader, e ){
 
             console.log( e );
-            console.log( up );
+            console.log( uploader );
 
         });
 
-        uploader.bind( 'FileUploaded', function( up, file, r ){
+        uploader.bind( 'FileUploaded', function( uploader, file, r ){
+
+            var input, data;
 
             r = $.parseJSON( r.response );
 
-            console.log( r );
-            // console.log( up );
-            // console.log( file );
+            if ( ! r.success ) {
+                console.log( 'error' );
+            }
 
+            // Remove the image from the loader & possibly hide the loader.
+            hide_image_loader( $loader, file );            
+
+            // Display the image
+            $images.append( r.data );
         });
 
     });
+
+    /**
+     * Return the message to be displayed when the max number of file uploads has been reach or exceeded.
+     *
+     * @param   int max_file_uploads
+     * @return  string
+     */
+    function get_max_uploads_message( max_file_uploads ) {
+        var msg = max_file_uploads > 1 ? CHARITABLE_UPLOAD_VARS.max_file_uploads_plural : CHARITABLE_UPLOAD_VARS.max_file_uploads_single;
+
+        return msg.replace( '%d', max_file_uploads );
+    };
+
+    /**
+     * Add an image loader bar to indicate that an image is being uploaded.
+     *
+     * @param   $loader
+     * @param   array file
+     * @return  void
+     */
+    function add_image_loader( $loader, file ) {
+        $loader.fadeIn( 300 )
+        $loader.children('.images').append( '<li data-file-id="' + file.id + '" class="">' + file.name + '</li>' );        
+    };
+
+    /**
+     * Hide the image loader.
+     *
+     * @param   $loader
+     * @param   array file
+     * @return  void
+     */
+    function hide_image_loader( $loader, file ) {
+        $loader.find( '[data-file-id=' + file.id + ']' ).remove();
+
+        if ( ! $loader.find('.images li').length ) {
+            $loader.hide();
+        }        
+    };
+
+    /**
+     * Remove an image.
+     *  
+     * @return  void
+     */
+    function remove_image() {
+        var $image = $(this).parent();
+
+        $image.fadeOut( 300, function(){
+            this.remove();
+        });
+
+        $image.parent().siblings( '.charitable-drag-drop-dropzone' ).fadeIn( 300 );
+
+        return false;
+    }
+
+    /**
+     * Hide the dropzone.
+     *
+     * @return  void
+     */
+    function hide_dropzone( $dropzone ) {
+        $dropzone.removeClass('drag-over').fadeOut( 300 );
+    }
 
     /**
      * Removes li element if there is an error with the file
