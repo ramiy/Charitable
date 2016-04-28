e2156ad6af3d1afa3bc04f9c9c04eff2075b94f0
Eric Daams <eric@164a.com>
2016-04-28 16:01:25 +0930

    Updated tested up to header in plugin head

--- charitable.php
+++ charitable.php
@@ -7,7 +7,7 @@
  * Author:              WP Charitable
  * Author URI:          https://wpcharitable.com
  * Requires at least:   4.1
- * Tested up to:        4.5
+ * Tested up to:        4.5.1
  *
  * Text Domain:         charitable
  * Domain Path:         /i18n/languages/
