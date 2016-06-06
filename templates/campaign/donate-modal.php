64ed6e36167410d7236f0459da0a495c62908480
Eric Daams <eric@164a.com>
2016-06-06 13:05:47 +0930

    Cleaner attribute definition

--- templates/campaign/donate-modal.php
+++ templates/campaign/donate-modal.php
@@ -14,7 +14,7 @@ $campaign = $view_args[ 'campaign' ];
     <a data-trigger-modal="charitable-donation-form-modal"
         class="donate-button button" 
         href="<?php echo charitable_get_permalink( 'campaign_donation_page', array( 'campaign_id' => $campaign->ID ) ) ?>" 
-        title="<?php echo esc_attr( sprintf( _x( 'Make a donation to %s', 'make a donation to campaign', 'charitable' ), get_the_title( $campaign->ID ) ) ) ?>">
+        title="<?php printf( esc_attr_x( 'Make a donation to %s', 'make a donation to campaign', 'charitable' ), get_the_title( $campaign->ID ) ) ?>">
     <?php _e( 'Donate', 'charitable' ) ?>
     </a>
 </div>
