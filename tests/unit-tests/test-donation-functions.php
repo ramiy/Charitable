932119c29dff5a5c675392d4d774efe316be2b05
Eric Daams <eric@164a.com>
2016-06-27 15:15:22 +0930

    Added callback function that checks whether a donation has been cancelled by the donor. If so, the donation status is changed. Closes #90

--- tests/unit-tests/test-donation-functions.php
+++ tests/unit-tests/test-donation-functions.php
@@ -1,90 +1,118 @@
 <?php
 
 /**
- * Contains tests for functions added in `includes/donations/charitable-donation-functions.php`. 
+ * Contains tests for functions added in `includes/donations/charitable-donation-functions.php`.
  */
 
 class Test_Charitable_Donation_Functions extends WP_UnitTestCase {
 
-    public function setUp() {
-        parent::setUp();
-    }
+	public function setUp() {
+		parent::setUp();
+	}
 
-    /**
-     * @covers charitable_create_donation
-     */
-    public function test_create_donation() {
+	/**
+	 * @covers charitable_create_donation
+	 */
+	public function test_create_donation() {
 
-        $donation_id = $this->create_donation();
+		$donation_id = $this->create_donation();
 
-        $this->assertGreaterThan( 0, $donation_id );
+		$this->assertGreaterThan( 0, $donation_id );
 
-    }
+	}
 
-    /**
-     * @covers charitable_create_donation
-     * @depends test_create_donation
-     */
-    public function test_create_donation_correct_gateway() {
+	/**
+	 * @covers charitable_create_donation
+	 * @depends test_create_donation
+	 */
+	public function test_create_donation_correct_gateway() {
 
-        $donation_id = $this->create_donation();
+		$donation_id = $this->create_donation();
 
-        $this->assertEquals( 'manual', charitable_get_donation_gateway( $donation_id ) );
+		$this->assertEquals( 'manual', charitable_get_donation_gateway( $donation_id ) );
 
-    }
+	}
 
-    /**
-     * @covers charitable_create_donation
-     * @depends test_create_donation
-     */
-    public function test_create_donation_correct_amount() {
+	/**
+	 * @covers charitable_create_donation
+	 * @depends test_create_donation
+	 */
+	public function test_create_donation_correct_amount() {
 
-        $donation_id = $this->create_donation();
+		$donation_id = $this->create_donation();
 
-        $this->assertEquals( 50, charitable_get_donation( $donation_id )->get_total_donation_amount( false ) );
+		$this->assertEquals( 50, charitable_get_donation( $donation_id )->get_total_donation_amount( false ) );
 
-    }
+	}
 
-    /**
-     * @covers charitable_create_donation
-     * @depends test_create_donation
-     */
-    public function test_create_donation_correct_user() {
+	/**
+	 * @covers charitable_create_donation
+	 * @depends test_create_donation
+	 */
+	public function test_create_donation_correct_user() {
 
-        $donation_id = $this->create_donation();
+		$donation_id = $this->create_donation();
 
-        $this->assertEquals( 'Matthew Murdoch', charitable_get_donation( $donation_id )->get_donor() );
+		$this->assertEquals( 'Matthew Murdoch', charitable_get_donation( $donation_id )->get_donor() );
 
-    }
+	}
 
-    /**
-     * Create a donation using `charitable_create_donation()`
-     *
-     * @return  int The donation ID
-     */
-    private function create_donation() {
+	/**
+	 * @covers charitable_cancel_donation
+	 * @depends test_create_donation
+	 */
+	private function test_cancel_donation() {
 
-        $campaign_id = Charitable_Campaign_Helper::create_campaign();
+		$donation_id = $this->create_donation( 'charitable-pending' );
 
-        $args = array(            
-            'status' => 'charitable-completed', 
-            'gateway' => 'manual', 
-            'note' => '',
-            'campaigns' => array(
-                array( 
-                    'campaign_id' => $campaign_id,
-                    'amount' => 50,
-                    'campaign_name' => 'Test Campaign'
-                )
-            ), 
-            'user' => array(
-                'first_name' => 'Matthew',
-                'last_name' => 'Murdoch',
-                'email' => 'matthew.murdoch@example.com'
-            )
-        );
+		set_query_var( 'cancel', true );
+		set_query_var( 'donation_id', $donation_id );
 
-        return charitable_create_donation( $args );
+		$this->assertTrue( charitable_cancel_donation() );
 
-    }
+	}
+
+	/**
+	 * @covers charitable_cancel_donation
+	 * @depends test_cancel_donation
+	 */
+	private function test_do_not_cancel_donation() {
+
+		$donation_id = $this->create_donation( 'charitable-pending' );
+
+		$this->assertFalse( charitable_cancel_donation() );
+
+	}
+
+	/**
+	 * Create a donation using `charitable_create_donation()`
+	 *
+	 * @param 	string $status
+	 * @return  int The donation ID
+	 */
+	private function create_donation( $status = 'charitable-completed' ) {
+
+		$campaign_id = Charitable_Campaign_Helper::create_campaign();
+
+		$args = array(
+			'status' => $status,
+			'gateway' => 'manual',
+			'note' => '',
+			'campaigns' => array(
+				array(
+					'campaign_id' => $campaign_id,
+					'amount' => 50,
+					'campaign_name' => 'Test Campaign',
+				),
+			),
+			'user' => array(
+				'first_name' => 'Matthew',
+				'last_name' => 'Murdoch',
+				'email' => 'matthew.murdoch@example.com',
+			),
+		);
+
+		return charitable_create_donation( $args );
+
+	}
 }
