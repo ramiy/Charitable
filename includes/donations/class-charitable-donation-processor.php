07a14821745c0997bedeb63a0c96afee50d2ae4c
Rafe Colton <rafael.colton@gmail.com>
2016-07-07 14:35:27 -0400

    Do not attempt to add non-existent donation to donation log
    If `charitable_get_donation()` returns something other than a donation,
    do not attempt to add it to the donation log.  Doing so, produces a
    500/server error.  Not doing so allows user to be returned to donation
    page with error displaying as expected

--- includes/donations/class-charitable-donation-processor.php
+++ includes/donations/class-charitable-donation-processor.php
@@ -530,7 +530,9 @@ if ( ! class_exists( 'Charitable_Donation_Processor' ) ) :
 		 */
 		public function update_donation_log( $donation_id, $message ) {
 			$donation = charitable_get_donation( $donation_id );
-			$donation->update_donation_log( $message );
+			if ( $donation ) {
+				$donation->update_donation_log( $message );
+			}
 		}
 
 		/**
