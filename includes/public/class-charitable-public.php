d80b79cec8f465097b9d20e62888db43c1eb4437
Eric Daams <eric@164a.com>
2016-07-28 20:25:28 +0930

    Added validation check to donation script to make sure that all required fields are filled out. Also handle failed AJAX requests in a nicer way. Closes #176 and closes #169

--- includes/public/class-charitable-public.php
+++ includes/public/class-charitable-public.php
@@ -122,6 +122,8 @@ if ( ! class_exists( 'Charitable_Public' ) ) :
 				'currency_format_thousand_sep' => esc_attr( charitable_get_option( 'thousands_separator', ',' ) ),
 				'currency_format' => esc_attr( charitable_get_currency_helper()->get_accounting_js_format() ), // For accounting.js
 				'error_invalid_amount' => sprintf( __( 'You must donate more than %s.', 'charitable' ), charitable_format_money( '0' ) ),
+				'error_required_fields' => __( 'Please fill out all required fields.', 'charitable' ),
+				'error_unknown' => __( 'Your donation could not be processed. Please reload the page and try again.', 'charitable' ),
 				'error_invalid_cc_number' => __( 'The credit card passed is not valid.', 'charitable' ),
 				'error_invalid_cc_expiry' => __( 'The credit card expiry date is not valid.', 'charitable' ),
 			) );
