b3f2d781758131791f468727bae317d0c0bc97b0
Eric Daams <eric@164a.com>
2016-07-29 14:33:19 +0930

    Add label support for optgroup elements in frontend select fields. Added escaping functions.

--- templates/shortcodes/registration.php
+++ templates/shortcodes/registration.php
@@ -10,7 +10,7 @@
 
 if ( ! defined( 'ABSPATH' ) ) { exit; } // Exit if accessed directly
 
-$form   = $view_args['form'];
+$form = $view_args['form'];
 
 /**
  * @hook    charitable_user_registration_before
