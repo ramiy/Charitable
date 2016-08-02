239e1b19613a39756d6e8a2aa96bcd353d9d8f85
Kathy Darling <helgatheviking@gmail.com>
2016-08-02 10:44:46 -0500

    add a clear button

--- includes/admin/views/donations-page/filters.php
+++ includes/admin/views/donations-page/filters.php
@@ -9,3 +9,8 @@
 
 ?>
 <a href="#charitable-donations-filter-modal" class="charitable-donations-filter button  dashicons-before dashicons-filter trigger-modal hide-if-no-js" data-trigger-modal><?php _e( 'Filter', 'charitable' ) ?></a>
+
+<?php if( isset ( $_GET["post_type"] ) && count( $_GET ) > 1 ) : ?>
+<a href="<?php echo esc_url( add_query_arg( array( "post_type" => Charitable::DONATION_POST_TYPE ), admin_url( "edit.php" ) ) );?>" class="charitable-donations-clear button dashicons-before dashicons-clear"><?php _e( 'Clear', 'charitable' ) ?></a>
+<?php endif; ?>
+
